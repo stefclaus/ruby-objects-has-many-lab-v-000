7a3d2403cc01d820d4f64d5750a161426f39ce61
Stef Claus <stef.claus@gmail.com>
2018-10-07 22:22:28 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,23 @@
 class Song 
-  attr_accessor :name 
+  attr_accessor :name, :artist 
   
   @@all = []
   
   def initialize(name)
     @name = name 
-    @@all << name
+    @@all << self 
   end 
   
   def self.all
     @@all 
+  end 
+  
+  def artist_name
+    if @artist != nil 
+      self.artist.name 
+    else 
+      nil 
+    end 
   end 
 
 
