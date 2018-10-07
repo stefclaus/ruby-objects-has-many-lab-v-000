b3b58c675d2cf77e28a44ab2274c0541cfc7bec0
Stef Claus <stef.claus@gmail.com>
2018-10-07 23:36:16 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,12 +1,14 @@
 class Author 
   attr_accessor :name, :posts
   
+  @@post_count = 0 
   def initialize
     @name = name 
   end 
   
   def add_post
     name.post = self
+    @@post_count += 1 
   end 
   
   def add_post_by_title
